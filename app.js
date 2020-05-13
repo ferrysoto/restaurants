5c44e1c6e3833d8eb5b6b29f6a10e6f2f185c2a3
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-05-14 00:06:33 +0200

    Restaurants create finished

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://webmaster:qwe123QWE@cluster0-kgkuk.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+//  Insertar restaurantes
 client.connect(err => {
-  const collection = client.db("restaurants").collection("devices");
+  const db = client.db("app");
+  const restaurantsCollection = db.collection("restaurants");
   // Comprobar que la conexión es correcta
   if (err) {
     console.log(err);
@@ -13,16 +20,21 @@ client.connect(err => {
     console.log("Connected to DB");
   }
 
-  client.close();
+  app.post('/restaurants', (req, res) => {
+    restaurantsCollection.insertOne(req.body)
+    .then(result => {
+      res.redirect('/')
+    })
+    .catch(error => console.error(error));
+  });
+
 });
 
 app.get('/', function (req, res){
   res.sendFile(__dirname  + '/index.html');
 });
 
-app.post('/restaurants', (req, res) => {
-  console.log(req.body);
-});
+
 
 app.listen(3000, function() {
   console.log('La app funciona en el puerto 3000');
